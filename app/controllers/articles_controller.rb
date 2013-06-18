d0962fab93f08c8f549e877490f97211e3f01334
unicornzero <rylee.k@gmail.com>
2013-06-18 14:31:23 -0600

    added edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,4 +24,14 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+    redirect_to article_path(@article)
+  end
+
 end
