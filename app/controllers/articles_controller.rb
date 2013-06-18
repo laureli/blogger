8f362c23d4289df6cd0d56a0c90a479e34a9df98
unicornzero <rylee.k@gmail.com>
2013-06-18 14:49:08 -0600

    added flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
   def create
   	@article = Article.new(params[:article])
   	@article.save
+    flash.notice = "Article '#{@article.title}' Created!"
   	redirect_to article_path(@article)
   end
 
@@ -21,6 +22,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    flash.notice = "Article '#{@article.title}' Deleted!"
     redirect_to articles_path
   end
 
@@ -31,6 +33,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update_attributes(params[:article])
+    flash.notice = "Article '#{@article.title}' Updated!"
     redirect_to article_path(@article)
   end
 
